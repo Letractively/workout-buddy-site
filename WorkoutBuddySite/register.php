00f15aeab5d64c843172b90ba931c31fa331b9cf
pensadov <pensadov@gmail.com>
2012-11-29 07:36:18 +0000

    Added "Forgot Password" to login. Added email field in registration. Added comments to registration fields.

--- WorkoutBuddySite/register.php
+++ WorkoutBuddySite/register.php
@@ -15,6 +15,7 @@
                 <input type="password" name="txtPassword" />
                 <input type="submit" class="button small" value="Login">
             </form>
+            <a href="#" class="textSmall">Forgot Password?</a>
         </div>
         <h1>Workout Buddy</h1>
         <ul id="barNav">
@@ -25,22 +26,32 @@
             <h2>Register</h2>
             <form id="frmRegister" action="dashboard.php" method="POST">
             	<div class="inputGroup">
-                    <label for="txtName">Your Name:</label>
+                    <label for="txtName">Full Name:</label>                    
                     <input type="text" name="txtName" />
+                    <label class="small">Helps your friends find you</label>
                 </div>
                 <div class="inputGroup">
-                    <label for="txtUsername">Username:</label>
+                    <label for="txtEmail">Email <span class="textSmall">(optional)</span>:</label>                    
+                    <input type="text" name="txtEmail" />
+                    <label class="small">To send notifications, never spam</label>
+                </div>
+                <div class="inputGroup">
+                    <label for="txtUsername">Username:</label>                    
                     <input type="text" name="txtUsername" />
+                    <label class="small">Used for logging in</label>
                 </div>
                 <div class="inputGroup">
-                    <label for="txtPassword">Password:</label>
+                    <label for="txtPassword">Password:</label>                    
                     <input type="password" name="txtPassword" />
+                    <label class="small">6 character minimum</label>
                 </div>
                 <div class="inputGroup">
-                    <label for="txtPassword2">Confirm Password:</label>
+                    <label for="txtPassword2">Confirm Password :</label>
                     <input type="password" name="txtPassword2" />
+                    <label class="small">Make sure you got it right</label>
                 </div>
                 <div class="inputGroup">
+                	<p></p>
                 	<input type="submit" class="button large" value="Register"> <a href="index.php">Cancel</a>
                 </div>
             </form>
