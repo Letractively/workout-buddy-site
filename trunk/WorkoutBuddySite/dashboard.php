f30b854abd87c5f0bc7005527c35bf06d12ca59f
pensadov@gmail.com <pensadov@gmail.com@91efb004-4057-2c6c-84fe-db1c79c06442>
2012-11-30 02:32:21 +0000

    Added pages for other tabs (empty for now)

--- trunk/WorkoutBuddySite/dashboard.php
+++ trunk/WorkoutBuddySite/dashboard.php
@@ -12,11 +12,11 @@
         </div>
         <h1>Workout Buddy</h1>
         <ul id="barNav">
-            <li><a class="current" href="#">Dashboard</a></li>
-            <li><a href="#">Progress</a></li>
-            <li><a href="#">Schedule</a></li>
-            <li><a href="#">Preferences</a></li>
-            <li><a href="#">Rewards</a></li>
+            <li><a class="current" href="dashboard.php">Dashboard</a></li>
+            <li><a href="progress.php">Progress</a></li>
+            <li><a href="schedule.php">Schedule</a></li>
+            <li><a href="preferences.php">Preferences</a></li>
+            <li><a href="rewards.php">Rewards</a></li>
         </ul>
         <div class="content">
             <table id="dashboard">
---
+++ trunk/WorkoutBuddySite/progress.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Dashboard - Workout Buddy</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+    <div class="main-container">
+    	<div id="barUser">
+               	Welcome, <strong>Peter</strong> <span class="textSmall">(<a href="index.php">Sign out</a>)</span>
+        </div>
+        <h1>Workout Buddy</h1>
+        <ul id="barNav">
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a class="current" href="progress.php">Progress</a></li>
+            <li><a href="schedule.php">Schedule</a></li>
+            <li><a href="preferences.php">Preferences</a></li>
+            <li><a href="rewards.php">Rewards</a></li>
+        </ul>
+        <div class="content">
+            <h3>Progress</h3>
+        </div>
+    </div>
+</body>
+</html>
