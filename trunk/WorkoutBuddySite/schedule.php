a0afd5ee4684fa9373432ee1bbe9a685ba529274
pensadov@gmail.com <pensadov@gmail.com@91efb004-4057-2c6c-84fe-db1c79c06442>
2012-11-30 03:11:28 +0000

    Added calendar

--- trunk/WorkoutBuddySite/schedule.php
+++ trunk/WorkoutBuddySite/schedule.php
@@ -1,8 +1,9 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Dashboard - Workout Buddy</title>
+    <title>Schedule - Workout Buddy</title>
     <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link rel="stylesheet" type="text/css" href="styles/schedule.css">
 </head>
 <body>
     <div class="main-container">
@@ -18,7 +19,16 @@
             <li><a href="rewards.php">Rewards</a></li>
         </ul>
         <div class="content">
-            <h3>Schedule</h3>
+            <h2 align="center">December 2012</h2>
+            <table border="1" align="center">
+                <tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>
+                <tr><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">1</span></td></tr>
+                <tr><td><span class="date">2</span></td><td><span class="date">3</span></td><td><span class="date">4</span></td><td><span class="date">5</span></td><td><span class="date">6</span></td><td><span class="date">7</span></td><td><span class="date">8</span></td></tr>
+                <tr><td><span class="date">9</span></td><td><span class="date">10</span></td><td><span class="date">11</span></td><td><span class="date">12</span></td><td><span class="date">13</span></td><td><span class="date">14</span></td><td><span class="date">15</span></td></tr>
+                <tr><td><span class="date">16</span></td><td><span class="date">17</span></td><td><span class="date">18</span></td><td><span class="date">19</span></td><td><span class="date">20</span></td><td><span class="date">21</span></td><td><span class="date">22</span></td></tr>
+                <tr><td><span class="date">23</span></td><td><span class="date">24</span></td><td><span class="date">25</span></td><td><span class="date">26</span></td><td><span class="date">27</span></td><td><span class="date">28</span></td><td><span class="date">29</span></td></tr>
+                <tr><td><span class="date">30</span></td><td><span class="date">31</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td><td><span class="date">&nbsp;</span></td></tr>
+            </table>
         </div>
     </div>
 </body>
