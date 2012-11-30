23788b35069b667e76bae2a31ec76812248c57ce
pensadov@gmail.com <pensadov@gmail.com@91efb004-4057-2c6c-84fe-db1c79c06442>
2012-11-30 02:52:51 +0000

    Added "Under Construction" page

---
+++ trunk/WorkoutBuddySite/under-construction.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Dashboard - Workout Buddy</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+    <div class="main-container">
+    	<div id="barUser">
+               	Welcome, <strong>Peter</strong> <span class="textSmall">(<a href="index.php">Sign out</a>)</span>
+        </div>
+        <h1>Workout Buddy</h1>
+        <ul id="barNav">
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="progress.php">Progress</a></li>
+            <li><a href="schedule.php">Schedule</a></li>
+            <li><a href="preferences.php">Preferences</a></li>
+            <li><a href="rewards.php">Rewards</a></li>
+        </ul>
+        <div class="content">
+            <h3>Under Construction</h3>
+            <p>Sorry, the page you are looking for is still under construction. Please go back and try something else.</p>
+        </div>
+    </div>
+</body>
+</html>
