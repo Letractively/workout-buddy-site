824b05efb4610e9e21552cdf9b8154a0ba77b57d
pensadov <pensadov@gmail.com>
2012-12-01 01:25:04 +0000

    Rewards page

--- WorkoutBuddySite/rewards.php
+++ WorkoutBuddySite/rewards.php
@@ -1,8 +1,9 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Dashboard - Workout Buddy</title>
+    <title>Rewards - Workout Buddy</title>
     <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link rel="stylesheet" type="text/css" href="styles/rewards.css">    
 </head>
 <body>
     <div class="main-container">
@@ -18,7 +19,29 @@
             <li><a class="current" href="rewards.php">Rewards</a></li>
         </ul>
         <div class="content">
-            <h3>Rewards</h3>
+            <h3>This Week's Rewards</h3>
+            <table>
+            	<tr>
+                	<td align="center"><img src="images/pullup.jpg"></td>
+                    <td>10% Off Home Pull-up Bar</td>
+                    <td align="center" width="25%"><a href="under-construction.php" class="button medium">Redeem</a></td>
+                </tr>
+                <tr>
+                	<td align="center"><img src="images/green-tea.jpg"></td>
+                    <td>1 Free Large Green tea at Tea House</td>
+                    <td align="center"><a href="under-construction.php" class="button medium">Redeem</a></td>
+                </tr>
+                <tr>
+                	<td align="center"><img src="images/tickets.jpg"></td>
+                    <td>Buy 1 Get 1 Free Movie Ticket</td>
+                    <td align="center"><a href="under-construction.php" class="button medium">Redeem</a></td>
+                </tr>
+                <tr>
+                	<td align="center"><img src="images/netflix.jpg"></td>
+                    <td>3 Month Netflix Subscription</td>
+                    <td align="center"><span class="textSmall">Sorry, your fitness level is not high enough for this reward (90% or higher required)</span></td>
+                </tr>
+            </table>
         </div>
     </div>
 </body>
