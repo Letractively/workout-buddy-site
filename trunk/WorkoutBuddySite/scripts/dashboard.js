36dfa6a8e23cdc881fb532d85047e7cebc241540
pensadov@gmail.com <pensadov@gmail.com@91efb004-4057-2c6c-84fe-db1c79c06442>
2012-12-03 08:26:30 +0000

    Added javascript notification to dashboard (run "notification()" on developer console)

---
+++ trunk/WorkoutBuddySite/scripts/dashboard.js
@@ -0,0 +1,37 @@
+function notification()
+{
+	setTimeout(function()
+	{
+		var notificationTable = $("#notificationTable");
+		var newRow = 
+		[
+			"<tr>",
+			"<td width=\"69%\"><a href=\"under-construction.html\">Oski</a> wants to register going to the gym with you.</td>",
+			"<td align=\"left\"><span class=\"textSmall\">Right Now</span></td>",
+			"<td align=\"right\"><a href=\"under-construction.html\" id=\"btnConfirm\" class=\"button small\">Confirm</a></td>",
+			"</tr>"
+		];
+		
+		notificationTable.fadeOut(120, function()
+		{
+			notificationTable.find("tbody").html(newRow.join(""));
+			notificationTable.fadeIn(120);
+			
+			var notificationHeader = $("#notificationHeader");
+			notificationHeader.text("Notifications (1)");
+			
+			$("#btnConfirm").click(function()
+			{
+				notificationTable.fadeOut(120, function()
+				{
+					notificationTable.find("tbody").html("<tr><td><span class=\"textSmall\">You have no notifications</span></td></tr>");
+					notificationTable.fadeIn(120);
+					
+					notificationHeader.text("Notifications");
+				});
+				
+				return false;
+			});
+		});
+	}, 2000);
+}
